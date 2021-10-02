3076dae3d9e3cf79dcfaf82e783fd872f5c9f5ce
Darsh <darsh@dxfactor.com>
2021-10-02 22:03:40 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -7,51 +7,43 @@ app = Flask(__name__)
 moves = ['F', 'T', 'L', 'R']
 
 
-def my_data(all_data):
-    my_name = all_data['_links']['self']['href']
-    my_coord = all_data["arena"]["state"][my_name]
+def get_bot(all_data):
+    bot = all_data['_links']['self']['href']
+    bot_state = all_data["arena"]["state"][bot]
 
-    return my_coord, my_name
+    return bot_state, bot
 
 
 
-def get_command(my_direction, my_coord_x, my_coord_y, arena_x, arena_y, all_user_data):
-    if my_direction == "N":
-        # X axis constant
-        # Y decreases as we move forward
-        if my_coord_y == 0:
+def get_command(bot_direction, bot_x, bot_y, arena_x, arena_y, all_user_data):
+    if bot_direction == "N":
+        if bot_y == 0:
             return "R"
-        elif is_someone_present([my_coord_x], [my_coord_y-1, my_coord_y-2, my_coord_y-3], all_user_data):
+        elif is_someone_present([bot_x], [bot_y-1, bot_y-2, bot_y-3], all_user_data):
             return "T"
         else:
             return "F"
 
-    elif my_direction == "S":
-        # X axis constant
-        # Y increases as we move forward
-        if my_coord_y == arena_y - 1:
+    elif bot_direction == "S":
+        if bot_y == arena_y - 1:
             return "R"
-        elif is_someone_present([my_coord_x], [my_coord_y+1, my_coord_y+2, my_coord_y+3], all_user_data):
+        elif is_someone_present([bot_x], [bot_y+1, bot_y+2, bot_y+3], all_user_data):
             return "T"
         else:
             return "F"
 
-    elif my_direction == "E":
-        # Y axis constant
-        # X increases as we move forward
-        if my_coord_x == arena_x - 1:
+    elif bot_direction == "E":
+        if bot_x == arena_x - 1:
             return "R"
-        elif is_someone_present([my_coord_x+1, my_coord_x+2, my_coord_x+3], [my_coord_y], all_user_data):
+        elif is_someone_present([bot_x+1, bot_x+2, bot_x+3], [bot_y], all_user_data):
             return "T"
         else:
             return "F"
 
-    elif my_direction == "W":
-        # Y axis constant
-        # X decreases as we move forward
-        if my_coord_x == 0:
+    elif bot_direction == "W":
+        if bot_x == 0:
             return "R"
-        elif is_someone_present([my_coord_x-1, my_coord_x-2, my_coord_x-3], [my_coord_y], all_user_data):
+        elif is_someone_present([bot_x-1, bot_x-2, bot_x-3], [bot_y], all_user_data):
             return "T"
         else:
             return "F"
@@ -60,8 +52,8 @@ def get_command(my_direction, my_coord_x, my_coord_y, arena_x, arena_y, all_user
 
 
 def is_someone_present(possible_points_x, possible_points_y, all_user_data):
-    for _each_user_name, each_user_stats in all_user_data.items():
-        if each_user_stats["x"] in possible_points_x and each_user_stats["y"] in possible_points_y:
+    for _, user_states in all_user_data.items():
+        if user_states["x"] in possible_points_x and user_states["y"] in possible_points_y:
             return True
     else:
         return False
@@ -70,10 +62,10 @@ def is_someone_present(possible_points_x, possible_points_y, all_user_data):
 @app.route("/", methods=['POST'])
 def move():
     request.get_data()
-    my_coord, my_name = my_data(request.json)
+    bot_state, _ = get_bot(request.json)
     arena_x, arena_y = request.json["arena"]["dims"]
-    return get_command(my_coord['direction'], my_coord['x'], my_coord['y'], arena_x, arena_y, request.json["arena"]["state"])
+    return get_command(bot_state['direction'], bot_state['x'], bot_state['y'], arena_x, arena_y, request.json["arena"]["state"])
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
