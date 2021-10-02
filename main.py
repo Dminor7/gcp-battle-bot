6e59120a53fbce9e1d965ff73162bf5948b84d0b
Darsh <darsh@dxfactor.com>
2021-10-02 19:02:24 +0530

    Adding develop

--- main.py
+++ main.py
@@ -1,201 +1,79 @@
 import os
-import logging
 import random
 from flask import Flask, request
-from collections import namedtuple
-from functools import cmp_to_key
-import random
 
-
-PlayerState = namedtuple('PlayerState', 'x y')
-PlayerState.__eq__ = lambda source, target: source.x == target.x and source.y == target.y
-
-DIRECTION_MAP = {
-    "N":1,
-    "E":2,
-    "S":3,
-    "W":4
-}
-
-def turn_left(vector: int):
-    result = vector - 1
-    if result < 0:
-        result = 4
-    return result
-
-def turn_right(vector: int):
-    result = vector + 1
-    if result > 4:
-        result = 1
-    return result
-
-
-def move_down(direction: str):
-    moves = {
-        "N":"R",
-        "E":"R",
-        "S":"F",
-        "W":"L"
-    }
-    return moves.get(direction,"F")
-
-def move_up(direction: str):
-    moves = {
-        "N":"F",
-        "E":"L",
-        "S":"R",
-        "W":"R"
-    }
-    return moves.get(direction,"F")
-
-def move_right(direction: str):
-    moves = {
-        "N":"R",
-        "E":"F",
-        "S":"L",
-        "W":"R"
-    }
-    return moves.get(direction,"F")
-
-def move_left(direction: str):
-    moves = {
-        "N":"L",
-        "E":"R",
-        "S":"R",
-        "W":"F"
-    }
-    return moves.get(direction,"F")
-
-def get_line(bot_state, vector):
-    line = [PlayerState(1,1)] * 3
-    if vector == 1:
-        line[0] = PlayerState(bot_state.get("x"),bot_state.get("y") - 1)
-        line[1] = PlayerState(bot_state.get("x"),bot_state.get("y") - 2)
-        line[2] = PlayerState(bot_state.get("x"),bot_state.get("y") - 3)
-    if vector == 2:
-        line[0] = PlayerState(bot_state.get("x"),bot_state.get("y") + 1)
-        line[1] = PlayerState(bot_state.get("x"),bot_state.get("y") + 2)
-        line[2] = PlayerState(bot_state.get("x"),bot_state.get("y") + 3)
-    if vector == 3:
-        line[0] = PlayerState(bot_state.get("x") + 1,bot_state.get("y"))
-        line[1] = PlayerState(bot_state.get("x") + 2,bot_state.get("y"))
-        line[2] = PlayerState(bot_state.get("x") + 3,bot_state.get("y"))
-    if vector == 4:
-        line[0] = PlayerState(bot_state.get("x") - 1,bot_state.get("y"))
-        line[1] = PlayerState(bot_state.get("x") - 2,bot_state.get("y"))
-        line[2] = PlayerState(bot_state.get("x") - 3,bot_state.get("y"))
-    return line
-    
-
-
-def find_bots_in_range(bot_state, arena, vector):
-    line = get_line(bot_state, vector)
-    n_line_bots = 0
-    player_states = [PlayerState(player_state.get("x"),player_state.get("y")) for player_state in arena.get("arena").get("state").values()]
-    for l in line:
-        n_line_bots += len(list(filter(lambda player_state: player_state == l,player_states)))
-    return n_line_bots
-
-def find_closest_bot(bot_state, arena):
-    def sort(player1 ,player2):
-        d1 = abs(bot_state.get("x") - player1.x) + abs(bot_state.get("y") -  player1.y)
-        d2 = abs(bot_state.get("x") - player2.x) + abs(bot_state.get("y") -  player2.y)
-        if d1 < d2:
-          return -1
-        elif d1 > d2:
-          return 1
-        else:
-          return 0
-
-    player_states = [PlayerState(player_state.get("x"),player_state.get("y")) for player_state in arena.get("arena").get("state").values()]
-    players = list(filter(lambda player: player != PlayerState(bot_state.get("x"), bot_state.get("y")),player_states))
-    players.sort(key=cmp_to_key(sort))
-    if len(players) > 0:
-        return players[0]
-    else:
-        return random.choice(player_states)
-
-
-    
-
-
-
-def make_a_move(arena):
-    bot = arena.get("_links").get("self").get("href")
-    bot_state = arena.get("arena").get("state").get(bot)
-    vector = DIRECTION_MAP.get(bot_state.get("direction"),1)
-    result_cmd = "T"
-    
-    max_bots_in_range = 0
-    bots_in_range = find_bots_in_range(bot_state, arena, vector)
-    if max_bots_in_range < bots_in_range:
-        max_bots_in_range = bots_in_range
-    
-    left_bots_in_range = find_bots_in_range(bot_state, arena, turn_left(vector))
-    if max_bots_in_range < left_bots_in_range:
-        max_bots_in_range = left_bots_in_range
-    
-    right_bots_in_range = find_bots_in_range(bot_state, arena, turn_right(vector))
-    if max_bots_in_range < right_bots_in_range:
-        max_bots_in_range = right_bots_in_range
-    
-    back_bots_in_range = find_bots_in_range(bot_state, arena, turn_left(turn_left(vector)))
-    if max_bots_in_range < back_bots_in_range:
-        max_bots_in_range = back_bots_in_range
-    
-    
-    if max_bots_in_range >= 0:
-        if max_bots_in_range == bots_in_range:
-            result_cmd = "T"
-        elif max_bots_in_range == left_bots_in_range:
-            result_cmd = "L"
-        elif max_bots_in_range == right_bots_in_range:
-            result_cmd = "R"
-        else:
-            result_cmd = "L"
-    
-    
-    else:
-        close_bot = find_closest_bot(bot_state, arena)
-        delta_x = bot_state.get("x") - close_bot.x
-        delta_y = bot_state.get("y") - close_bot.y
-        bot_direction = bot_state.get("direction")
-        if delta_y == 0:
-            if delta_x > 3:
-                result_cmd = move_right(bot_direction)
-            else:
-                result_cmd = move_left(bot_direction)
-        elif delta_x == 0:
-            if delta_y > 3:
-                result_cmd = move_down(bot_direction)
-            else:
-                result_cmd = move_up(bot_direction)
-        elif delta_x < delta_y:
-            if delta_x > 3:
-                result_cmd = move_right(bot_direction)        
-            else:
-                result_cmd = move_left(bot_direction)
-        else:
-            if delta_y > 3:
-                result_cmd = move_down(bot_direction)
-            else:
-                result_cmd = move_up(bot_direction)
-
-    return result_cmd
-
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+moves = ['F', 'T', 'L', 'R']
+
+
+def my_data(all_data):
+    my_name = all_data['_links']['self']['href']
+    my_coord = all_data["arena"]["state"][my_name]
+
+    return my_coord, my_name
+
+
+
+def get_command(my_direction, my_coord_x, my_coord_y, arena_x, arena_y, all_user_data):
+    if my_direction == "N":
+        # X axis constant
+        # Y decreases as we move forward
+        if my_coord_y == 0:
+            return "R"
+        elif is_someone_present([my_coord_x], [my_coord_y-1, my_coord_y-2, my_coord_y-3], all_user_data):
+            return "T"
+        else:
+            return "F"
+
+    elif my_direction == "S":
+        # X axis constant
+        # Y increases as we move forward
+        if my_coord_y == arena_y - 1:
+            return "R"
+        elif is_someone_present([my_coord_x], [my_coord_y+1, my_coord_y+2, my_coord_y+3], all_user_data):
+            return "T"
+        else:
+            return "F"
+
+    elif my_direction == "E":
+        # Y axis constant
+        # X increases as we move forward
+        if my_coord_x == arena_x - 1:
+            return "R"
+        elif is_someone_present([my_coord_x+1, my_coord_x+2, my_coord_x+3], [my_coord_y], all_user_data):
+            return "T"
+        else:
+            return "F"
+
+    elif my_direction == "W":
+        # Y axis constant
+        # X decreases as we move forward
+        if my_coord_x == 0:
+            return "R"
+        elif is_someone_present([my_coord_x-1, my_coord_x-2, my_coord_x-3], [my_coord_y], all_user_data):
+            return "T"
+        else:
+            return "F"
+    else:
+        return moves[random.randrange(len(moves))]
+
+
+def is_someone_present(possible_points_x, possible_points_y, all_user_data):
+    for _each_user_name, each_user_stats in all_user_data.items():
+        if each_user_stats["x"] in possible_points_x and each_user_stats["y"] in possible_points_y:
+            return True
+    else:
+        return False
 
 
 @app.route("/", methods=['POST'])
 def move():
-    data = request.json
-    bot_move = make_a_move(arena=data)
-    
+    request.get_data()
+    my_coord, my_name = my_data(request.json)
+    arena_x, arena_y = request.json["arena"]["dims"]
+    return get_command(my_coord['direction'], my_coord['x'], my_coord['y'], arena_x, arena_y, request.json["arena"]["state"])
 
-    return bot_move
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=False, host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
